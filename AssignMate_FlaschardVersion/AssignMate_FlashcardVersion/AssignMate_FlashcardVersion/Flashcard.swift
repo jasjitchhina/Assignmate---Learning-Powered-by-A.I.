a8ad442f29ca766c42cbd08220e1bfde0ec41696
lukeborozan <147724303+lukeborozan@users.noreply.github.com>
2023-10-26 17:56:49 -0400

    Add files via upload

---
+++ AssignMate_FlaschardVersion/AssignMate_FlashcardVersion/AssignMate_FlashcardVersion/Flashcard.swift
@@ -0,0 +1,36 @@
+//
+//  Flashcard.swift
+//  AssignMate_FlashcardVersion
+//
+//  Created by Peter Borozan on 10/25/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct Flashcard<Front: View, Back: View>: View {
+    var front: () -> Front
+    var back: () -> Back
+
+    @State private var isFlipped = false
+
+    var body: some View {
+        let side = isFlipped ? back() as! Front : front()
+
+        return ZStack {
+            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                .fill(Color.white)
+                .shadow(color: .gray, radius: 10, x: 0, y: 0)
+
+            side.padding(20)
+        }
+        .aspectRatio(3/2, contentMode: .fit)
+        .rotationEffect(.degrees(90))
+        .frame(width: 400, height: 300)
+        .onTapGesture {
+            withAnimation {
+                self.isFlipped.toggle()
+            }
+        }
+    }
+}
