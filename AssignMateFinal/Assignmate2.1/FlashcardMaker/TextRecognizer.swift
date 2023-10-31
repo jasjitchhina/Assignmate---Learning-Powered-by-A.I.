c7ef7fa83e21656b68d0b2a33a251ad33408e97a
lukeborozan <147724303+lukeborozan@users.noreply.github.com>
2023-10-30 20:58:57 -0400

    TextRecognizer.swift

--- AssignMateFinal/Assignmate2.1/FlashcardMaker/TextRecognizer.swift
+++ AssignMateFinal/Assignmate2.1/FlashcardMaker/TextRecognizer.swift
@@ -1,38 +1,44 @@
-//
-//  TextRecognizer.swift
-//  AssignMateFLASHCARD+OCR
-//
-//  Created by Peter Borozan on 10/28/23.
-//
 
 import Foundation
 import Vision
 import VisionKit
 
-final class TextRecognizer{
+// A class responsible for recognizing text from scanned images
+final class TextRecognizer {
     let cameraScan: VNDocumentCameraScan
-    init(cameraScan:VNDocumentCameraScan) {
+    
+    init(cameraScan: VNDocumentCameraScan) {
         self.cameraScan = cameraScan
     }
-    private let queue = DispatchQueue(label: "scan-codes",qos: .default,attributes: [],autoreleaseFrequency: .workItem)
-    func recognizeText(withCompletionHandler completionHandler:@escaping ([String])-> Void) {
+    
+    // Queue for processing text recognition requests
+    private let queue = DispatchQueue(label: "scan-codes", qos: .default, attributes: [], autoreleaseFrequency: .workItem)
+    
+    // Function to recognize text from scanned images
+    func recognizeText(withCompletionHandler completionHandler: @escaping ([String]) -> Void) {
         queue.async {
-            let images = (0..<self.cameraScan.pageCount).compactMap({
+            // Extract images from each scanned page
+            let images = (0..<self.cameraScan.pageCount).compactMap {
                 self.cameraScan.imageOfPage(at: $0).cgImage
-            })
-            let imagesAndRequests = images.map({(image: $0, request:VNRecognizeTextRequest())})
-            let textPerPage = imagesAndRequests.map{image,request->String in
+            }
+            
+            // Create an array of images and text recognition requests
+            let imagesAndRequests = images.map { (image: $0, request: VNRecognizeTextRequest()) }
+            
+            // Process each image and extract text
+            let textPerPage = imagesAndRequests.map { image, request -> String in
                 let handler = VNImageRequestHandler(cgImage: image, options: [:])
-                do{
+                do {
                     try handler.perform([request])
-                    guard let observations = request.results else{return ""}
-                    return observations.compactMap({$0.topCandidates(1).first?.string}).joined(separator: "\n")
-                }
-                catch{
+                    guard let observations = request.results else { return "" }
+                    return observations.compactMap { $0.topCandidates(1).first?.string }.joined(separator: "\n")
+                } catch {
                     print(error)
                     return ""
                 }
             }
+            
+            // Notify the main thread with the recognized text
             DispatchQueue.main.async {
                 completionHandler(textPerPage)
             }
