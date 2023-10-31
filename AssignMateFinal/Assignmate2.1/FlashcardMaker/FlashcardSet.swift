bba341f066a3042833d87bcbd3793067569a7f9f
lukeborozan <147724303+lukeborozan@users.noreply.github.com>
2023-10-30 20:40:40 -0400

    FlashcardSet.swift

--- AssignMateFinal/Assignmate2.1/FlashcardMaker/FlashcardSet.swift
+++ AssignMateFinal/Assignmate2.1/FlashcardMaker/FlashcardSet.swift
@@ -1,7 +1,16 @@
+// Import necessary modules
 import Foundation
 
+// Define a Flashcard set data model
 struct FlashcardSet: Identifiable, Codable {
+    
+    // Unique identifier for each flashcard set, using String data type
     var id: String  // Use String instead of UUID
+    
+    // Title of the flashcard set
     var title: String
+    
+    // Collection of flashcards within this set
     var cards: [FlashcardData]
 }
+
