d10fb88b4111274b17ef65f872e7c21d3156b590
lukeborozan <147724303+lukeborozan@users.noreply.github.com>
2023-10-30 21:00:11 -0400

    Users.swift

--- AssignMateFinal/Assignmate2.1/Models/Users.swift
+++ AssignMateFinal/Assignmate2.1/Models/Users.swift
@@ -1,16 +1,10 @@
-//
-//  Users.swift
-//  Assignmate2.1
-//
-//  Created by Jesse Chhina on 10/16/23.
-//
 
 import Foundation
 
+// A struct representing a user
 struct User: Codable {
-    let id: String
-    let name: String
-    let email: String
-    let joined: TimeInterval
-    
+    let id: String          // Unique identifier for the user
+    let name: String        // User's name
+    let email: String       // User's email address
+    let joined: TimeInterval // Date when the user joined (in seconds since reference date)
 }
