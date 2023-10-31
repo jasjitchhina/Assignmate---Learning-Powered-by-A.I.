791a16b9e4b758a93bb942b8f29f5a185d9707a6
lukeborozan <147724303+lukeborozan@users.noreply.github.com>
2023-10-30 21:34:46 -0400

    MainView.swift

--- AssignMateFinal/Assignmate2.1/Views/MainView.swift
+++ AssignMateFinal/Assignmate2.1/Views/MainView.swift
@@ -1,54 +1,55 @@
-//
-//  ContentView.swift
-//  AssignMate
-//
-//  Created by Jesse Chhina on 10/13/23.
-//
-
 import SwiftUI
 
+// The main view of the application
 struct MainView: View {
+    // State objects and environment objects
     @StateObject private var flashcardStore = FlashcardStore()
     @StateObject var viewModel = MainViewViewModel()
     @Environment(\.colorScheme) var colorScheme
     
-    
     var body: some View {
+        // Check if the user is signed in and the current user ID is not empty
         if viewModel.isSignedIn, !viewModel.currentUserId.isEmpty {
-            accountView
-        } else{
-            LoginView()
+            accountView // Show the account view
+        } else {
+            LoginView() // Show the login view
         }
     }
     
+    // Define the account view with tab navigation
     @ViewBuilder
     var accountView: some View {
         ZStack {
-            // This will automatically pick the right color based on the current color scheme
+            // Set the background color based on the current color scheme
             Color("BackGroundColor").ignoresSafeArea()
 
             TabView {
+                // Assignments view for managing assignments
                 AssignmentsView(userId: viewModel.currentUserId)
-                    .tabItem{
+                    .tabItem {
                         Label("Assignments", systemImage: "list.bullet.clipboard")
                     }
+                
+                // Flashcard generator view
                 MakeFlashcardsView()
                     .environmentObject(flashcardStore)
                     .tabItem {
                         Label("Flashcard Generator", systemImage: "rectangle.and.pencil.and.ellipsis")
                     }
+                
+                // View for managing saved flashcards
                 SavedFlashcardsView()
                     .environmentObject(FlashcardStore())
                     .tabItem {
                         Label("Your Flashcards", systemImage: "sparkles.rectangle.stack.fill")
                     }
+                
+                // User profile view
                 ProfileView()
-                    .tabItem{
+                    .tabItem {
                         Label("Profile", systemImage: "person.circle")
                     }
-            
             }
         }
     }
-
 }
