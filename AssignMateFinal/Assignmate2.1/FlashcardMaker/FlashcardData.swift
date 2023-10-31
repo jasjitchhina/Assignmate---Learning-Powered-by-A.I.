3c5981199e0b40c713ce719a935e57fb49c34e19
lukeborozan <147724303+lukeborozan@users.noreply.github.com>
2023-10-30 20:39:52 -0400

    FlashcardData.swift

--- AssignMateFinal/Assignmate2.1/FlashcardMaker/FlashcardData.swift
+++ AssignMateFinal/Assignmate2.1/FlashcardMaker/FlashcardData.swift
@@ -1,15 +1,15 @@
-//
-//  FlashcardData.swift
-//  AssignMateFLASHCARD+OCR
-//
-//  Created by Peter Borozan on 10/28/23.
-//
-
+// Import necessary modules
 import Foundation
 
 // Define a Flashcard data model
 struct FlashcardData: Identifiable, Codable {
+    
+    // Unique identifier for each flashcard, generated using UUID
     var id = UUID()
+    
+    // Text content for the front side of the flashcard
     var front: String
+    
+    // Text content for the back side of the flashcard
     var back: String
 }
