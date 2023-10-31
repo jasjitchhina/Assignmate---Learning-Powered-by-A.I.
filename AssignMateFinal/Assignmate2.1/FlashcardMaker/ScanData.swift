9e07a5027c43d116acefa3ba07fa1f8f6939433a
lukeborozan <147724303+lukeborozan@users.noreply.github.com>
2023-10-30 20:55:47 -0400

    ScanData.swift

--- AssignMateFinal/Assignmate2.1/FlashcardMaker/ScanData.swift
+++ AssignMateFinal/Assignmate2.1/FlashcardMaker/ScanData.swift
@@ -1,18 +1,12 @@
-//
-//  ScanData.swift
-//  AssignMateFLASHCARD+OCR
-//
-//  Created by Peter Borozan on 10/28/23.
-//
-
 import Foundation
 
-
-struct ScanData:Identifiable {
-    var id = UUID()
-    let content:String
+// A struct to represent scanned data
+struct ScanData: Identifiable {
+    var id = UUID()        // Unique identifier for each ScanData instance
+    let content: String    // The content of the scanned data
     
-    init(content:String) {
+    // Initialize a ScanData instance with the provided content
+    init(content: String) {
         self.content = content
     }
 }
