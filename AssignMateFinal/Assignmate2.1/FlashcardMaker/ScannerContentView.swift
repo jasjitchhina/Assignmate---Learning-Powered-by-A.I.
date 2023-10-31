69957300eaa3976a0ae1d8c10361fc725449a633
lukeborozan <147724303+lukeborozan@users.noreply.github.com>
2023-10-30 20:56:33 -0400

    ScannerContentView.swift

--- AssignMateFinal/Assignmate2.1/FlashcardMaker/ScannerContentView.swift
+++ AssignMateFinal/Assignmate2.1/FlashcardMaker/ScannerContentView.swift
@@ -1,47 +1,43 @@
-//
-//  ScannerContentView.swift
-//  AssignMateFLASHCARD+OCR
-//
-//  Created by Peter Borozan on 10/28/23.
-//
-
 
 import SwiftUI
 
 struct ScannerContentView: View {
     @State private var showScannerSheet = false
-    @State private var texts:[ScanData] = []
+    @State private var texts: [ScanData] = []
+    
     var body: some View {
-        NavigationView{
-            VStack{
-                if texts.count > 0{
-                    List{
-                        ForEach(texts){text in
+        NavigationView {
+            VStack {
+                if texts.count > 0 {
+                    // Display a list of scanned texts
+                    List {
+                        ForEach(texts) { text in
                             NavigationLink(
-                                destination:ScrollView{Text(text.content)},
+                                destination: ScrollView { Text(text.content) },
                                 label: {
                                     Text(text.content).lineLimit(1)
-                                })
+                                }
+                            )
                         }
                     }
-                }
-                else{
+                } else {
                     Text("No scan yet").font(.title)
                 }
             }
-                .navigationTitle("Scan OCR")
-                .navigationBarItems(trailing: Button(action: {
-                    self.showScannerSheet = true
-                }, label: {
-                    Image(systemName: "doc.text.viewfinder")
-                        .font(.title)
-                })
-                .sheet(isPresented: $showScannerSheet, content: {
-                    self.makeScannerView()
-                })
-                )
+            .navigationTitle("Scan OCR")
+            .navigationBarItems(trailing: Button(action: {
+                self.showScannerSheet = true
+            }, label: {
+                Image(systemName: "doc.text.viewfinder")
+                    .font(.title)
+            })
+            .sheet(isPresented: $showScannerSheet, content: {
+                self.makeScannerView()
+            }))
         }
     }
+    
+    // Function to create the ScannerView
     private func makeScannerView() -> some View {
         return ScannerView(completion: { textPerPage in
             if let outputText = textPerPage?.joined(separator: "\n").trimmingCharacters(in: .whitespacesAndNewlines) {
