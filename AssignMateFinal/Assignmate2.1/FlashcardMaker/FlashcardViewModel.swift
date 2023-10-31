3f48bf6b8effb16b87bfb5b47b4a18c8006d0741
lukeborozan <147724303+lukeborozan@users.noreply.github.com>
2023-10-30 20:50:17 -0400

    FlashcardViewModel.swift

--- AssignMateFinal/Assignmate2.1/FlashcardMaker/FlashcardViewModel.swift
+++ AssignMateFinal/Assignmate2.1/FlashcardMaker/FlashcardViewModel.swift
@@ -6,37 +6,38 @@ import SwiftUI
 import Combine
 import OpenAI
 
+// This ViewModel is responsible for handling business logic related to flashcards.
 class FlashcardViewModel: ObservableObject {
-    @Published var flashcards: [FlashcardData] = []
-    @Published var inputText: String = ""
-    @Published var navigate: Bool = false
-    @Published var currentFlashcardSet: FlashcardSet?
-    @Published var isLoading: Bool = true  // Reflect loading state
+    // These properties will be observed by the SwiftUI view for changes.
+    @Published var flashcards: [FlashcardData] = []       // List of flashcards generated from the input text.
+    @Published var inputText: String = ""                 // User's input text from which flashcards are generated.
+    @Published var navigate: Bool = false                 // Navigation flag, possibly for navigating to a new screen/view.
+    @Published var currentFlashcardSet: FlashcardSet?     // Holds the current set of flashcards.
+    @Published var isLoading: Bool = true                 // Reflects the loading state for asynchronous operations.
 
-    
-    private var db = Firestore.firestore()
+    private var db = Firestore.firestore()                // Firestore database instance.
 
+    // Creates a new flashcard set from provided flashcards and title.
     func createFlashcardSet(from flashcards: [FlashcardData], title: String) {
         guard let userId = Auth.auth().currentUser?.uid, !userId.isEmpty else {
             print("Error: userId is nil or empty.")
             return
         }
-        
-        
-        // Generating a UUID for the new flashcard set
-        let id = UUID().uuidString
 
-        // Creating a new flashcard set with the generated UUID
+        let id = UUID().uuidString
         let newSet = FlashcardSet(id: id, title: title, cards: flashcards)
         self.currentFlashcardSet = newSet
         
-        // Save this flashcard set to Firebase
+        // Persist the flashcard set to Firebase.
         saveFlashcardSetToFirebase(flashcardSet: newSet, userId: userId)
     }
-    
+
+    // Method to reset the navigation flag.
     func resetNavigation() {
         navigate = false
     }
+
+    // Triggers the process of generating flashcards from the input text.
     func sendText() {
         if !inputText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
             Task {
@@ -45,12 +46,12 @@ class FlashcardViewModel: ObservableObject {
         }
     }
 
-
     private let openAI = OpenAI(apiToken: "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")
-    
+
+    // Load flashcards from OpenAI based on the input text.
     func loadFlashcards() async {
         do {
-            // Set isLoading on the main thread
+            // Indicate loading state to UI.
             DispatchQueue.main.async { [weak self] in
                 self?.isLoading = true
             }
@@ -58,22 +59,21 @@ class FlashcardViewModel: ObservableObject {
             let response: String = try await requestFlashcard()
             let newFlashcards = parseResponseIntoFlashcards(response: response)
 
-            // Update UI-related properties on the main thread
+            // Update UI properties on the main thread after processing is complete.
             DispatchQueue.main.async { [weak self] in
                 self?.flashcards = newFlashcards
                 self?.isLoading = false
-                self?.navigate = true  // Navigate after loading
+                self?.navigate = true  // Trigger navigation after loading.
             }
         } catch {
             print("An error occurred: \(error)")
-            
-            // Handle error and update UI on the main thread
             DispatchQueue.main.async { [weak self] in
                 self?.isLoading = false
-                // Additionally, consider updating some error state property here
             }
         }
     }
+
+    // Sends a chat request to OpenAI and gets back a response in flashcard format.
     private func requestFlashcard() async throws -> String {
         let query = ChatQuery(
             model: "ft:gpt-3.5-turbo-0613:personal::8E1HWzrz",
@@ -91,6 +91,7 @@ class FlashcardViewModel: ObservableObject {
         }
     }
 
+    // Parse the response from OpenAI into individual flashcards.
     private func parseResponseIntoFlashcards(response: String) -> [FlashcardData] {
         let flashcardBlocks = response.components(separatedBy: "\n\n")
         return flashcardBlocks.compactMap { block in
@@ -103,16 +104,17 @@ class FlashcardViewModel: ObservableObject {
             return nil
         }
     }
-    
-    private func saveFlashcardSetToFirebase(flashcardSet: FlashcardSet, userId: String) {
-        // Generating a unique ID for the flashcard set
-        let id = UUID().uuidString
 
-        // Convert your flashcard set to dictionary for Firestore
+    // Persists the provided flashcard set to Firestore.
+    private func saveFlashcardSetToFirebase(flashcardSet: FlashcardSet, userId: String) {
+        let id = UUID().uuidString
         let setData: [String: Any] = [
-            "id": id,
+            "id": flashcardSet.id,
             "title": flashcardSet.title,
-            "cards": flashcardSet.cards.map { ["front": $0.front, "back": $0.back] }
+            "cards": flashcardSet.cards.map { [
+                "front": $0.front,
+                "back": $0.back
+            ] }
         ]
 
         db.collection("users").document(userId).collection("flashcardSets").addDocument(data: setData) { error in
