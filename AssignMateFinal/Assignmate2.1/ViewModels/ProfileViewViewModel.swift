7aa243c05ac0d718c8349fec64a8fb5475700ed1
lukeborozan <147724303+lukeborozan@users.noreply.github.com>
2023-10-30 21:07:43 -0400

    ProfileViewViewModel.swift

--- AssignMateFinal/Assignmate2.1/ViewModels/ProfileViewViewModel.swift
+++ AssignMateFinal/Assignmate2.1/ViewModels/ProfileViewViewModel.swift
@@ -1,28 +1,30 @@
-//
-//  ProfileViewViewModel.swift
-//  AssignMate
-//
-//  Created by Jesse Chhina on 10/13/23.
-//
+
 import FirebaseAuth
 import FirebaseFirestore
-
 import Foundation
 
-class ProfileViewViewModel: ObservableObject{
-    init(){}
+// A view model class for managing the user profile view
+class ProfileViewViewModel: ObservableObject {
+    init() {}
+    
     @Published var user: User? = nil
     
-    func fetchUser(){
-        guard let userId = Auth.auth().currentUser?.uid else{
+    // Function to fetch user data
+    func fetchUser() {
+        // Get the current user's UID
+        guard let userId = Auth.auth().currentUser?.uid else {
             return
         }
+        
+        // Access Firestore database
         let db = Firestore.firestore()
-        db.collection("users").document(userId).getDocument{[weak self] snapshot, error in
-            guard let data = snapshot?.data(), error == nil else{
+        db.collection("users").document(userId).getDocument { [weak self] snapshot, error in
+            guard let data = snapshot?.data(), error == nil else {
                 return
             }
-            DispatchQueue.main.async{
+            
+            DispatchQueue.main.async {
+                // Create a user object with fetched data
                 self?.user = User(
                     id: data["id"] as? String ?? "",
                     name: data["name"] as? String ?? "",
@@ -32,17 +34,13 @@ class ProfileViewViewModel: ObservableObject{
             }
         }
     }
-        
-    func logOut(){
-        do{
+    
+    // Function to log out the current user
+    func logOut() {
+        do {
             try Auth.auth().signOut()
-        } catch{
+        } catch {
             print(error)
         }
-        
-        
-        }
-        
     }
-    
-
+}
