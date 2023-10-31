a4350e61cc22f5f72c6736008e727ef1b504226c
lukeborozan <147724303+lukeborozan@users.noreply.github.com>
2023-10-30 21:31:35 -0400

    AMButton.swift

--- AssignMateFinal/Assignmate2.1/Views/AMButton.swift
+++ AssignMateFinal/Assignmate2.1/Views/AMButton.swift
@@ -1,40 +1,36 @@
-//
-//  AMButton.swift
-//  AssignMate
-//
-//  Created by Jesse Chhina on 10/14/23.
-//
 
 import SwiftUI
 
+// A custom button view
 struct AMButton: View {
     let title: String
     let background: Color
     let action: () -> Void
     
-    
     var body: some View {
         Button(
             action: {
                 action()
-            }
-            , label: {
-                ZStack{
+            },
+            label: {
+                ZStack {
                     RoundedRectangle(cornerRadius: 10)
-                        . foregroundColor(background)
-                
+                        .foregroundColor(background)
+                    
                     Text(title)
                         .foregroundStyle(Color.white)
-                        . bold()
-                    
+                        .bold()
                 }
             })
     }
 }
 
-#Preview {
-    AMButton(title: "Value",
-             background: .blue) {
-        //action
+// A preview of the custom button
+struct AMButton_Previews: PreviewProvider {
+    static var previews: some View {
+        AMButton(title: "Value",
+                 background: .blue) {
+            // Action to perform when the button is tapped
+        }
     }
 }
