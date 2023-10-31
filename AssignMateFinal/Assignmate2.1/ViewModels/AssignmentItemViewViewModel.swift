76aced6630a14d66022796ed8fe9495c1390bc9c
lukeborozan <147724303+lukeborozan@users.noreply.github.com>
2023-10-30 21:02:58 -0400

    AssignmentItemViewViewModel.swift

--- AssignMateFinal/Assignmate2.1/ViewModels/AssignmentItemViewViewModel.swift
+++ AssignMateFinal/Assignmate2.1/ViewModels/AssignmentItemViewViewModel.swift
@@ -1,39 +1,40 @@
-//
-//  AssignmentItemViewViewModel.swift
-//  AssignMate
-//
-//  Created by Jesse Chhina on 10/13/23.
-//
 
 import FirebaseAuth
 import FirebaseFirestore
 import Foundation
 
+// A view model class for managing assignment item data
 class AssignmentItemViewViewModel: ObservableObject {
     
     init() {}
     
+    // Function to toggle the 'isDone' property of an assignment item
     func toggleIsDone(item: AssignmentItem) {
         var itemCopy = item
         itemCopy.setDone(!item.isDone)
         
+        // Get the current user's UID
         guard let uid = Auth.auth().currentUser?.uid else {
             return
         }
         
+        // Access Firestore database
         let db = Firestore.firestore()
         db.collection("users")
             .document(uid)
             .collection("assignments")
             .document(itemCopy.id)
-            .setData(itemCopy.asDictionary())
+            .setData(itemCopy.asDictionary()) // Update the assignment item in Firestore
     }
     
+    // Function to remove an assignment item
     func removeItem(item: AssignmentItem) {
+        // Get the current user's UID
         guard let uid = Auth.auth().currentUser?.uid else {
             return
         }
         
+        // Access Firestore database
         let db = Firestore.firestore()
         db.collection("users")
             .document(uid)
