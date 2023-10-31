f3e928307aba7f75873530c6878b035254abf5b2
lukeborozan <147724303+lukeborozan@users.noreply.github.com>
2023-10-30 21:36:14 -0400

    RegisterView.swift

--- AssignMateFinal/Assignmate2.1/Views/RegisterView.swift
+++ AssignMateFinal/Assignmate2.1/Views/RegisterView.swift
@@ -1,48 +1,36 @@
-//
-//  RegisterView.swift
-//  AssignMate
-//
-//  Created by Jesse Chhina on 10/13/23.
-//
-
 import SwiftUI
 
 struct RegisterView: View {
     @StateObject var viewModel = RegisterViewViewModel()
     
     var body: some View {
-        VStack{
-            //header
-           HeaderView(title: "Register",
-                    subtitle: "Start supercharging your learning.",
-                            angle: -15,
+        VStack {
+            // Header
+            HeaderView(title: "Register",
+                        subtitle: "Start supercharging your learning.",
+                        angle: -15,
                         background: .blue)
             
-            Form{
+            Form {
                 TextField("Full Name", text: $viewModel.name)
-                    . textFieldStyle(DefaultTextFieldStyle())
+                    .textFieldStyle(DefaultTextFieldStyle())
                     .autocorrectionDisabled()
 
                 TextField("Email Address", text: $viewModel.email)
-                    . textFieldStyle(DefaultTextFieldStyle())
+                    .textFieldStyle(DefaultTextFieldStyle())
                     .autocorrectionDisabled()
                     .autocapitalization(.none)
-                
-                
+
                 SecureField("Password", text: $viewModel.password)
-                    . textFieldStyle(DefaultTextFieldStyle())
-             
-                AMButton(title: "Create Account",
-                         background: .green)
-                {
+                    .textFieldStyle(DefaultTextFieldStyle())
+
+                AMButton(title: "Create Account", background: .green) {
                     viewModel.register()
                 }
-                
             }
-            .offset(y: -50)
+            .offset(y: -50) // Adjust the vertical offset of the form
             
-            Spacer()
-            
+            Spacer() // Add spacer to push content to the top of the view
         }
     }
 }
