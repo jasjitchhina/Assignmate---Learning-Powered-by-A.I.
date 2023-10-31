f3f7c27ad0997bd48e4b69aadb6db411000954cd
lukeborozan <147724303+lukeborozan@users.noreply.github.com>
2023-10-30 21:37:53 -0400

    Assignmate2_1Tests.swift

--- AssignMateFinal/Assignmate2.1Tests/Assignmate2_1Tests.swift
+++ AssignMateFinal/Assignmate2.1Tests/Assignmate2_1Tests.swift
@@ -1,36 +1,40 @@
-//
-//  Assignmate2_1Tests.swift
-//  Assignmate2.1Tests
-//
-//  Created by Jesse Chhina on 10/16/23.
-//
-
+// Import XCTest framework for unit testing
 import XCTest
+
+// Import the app target for testing, allowing access to internal components
 @testable import Assignmate2_1
 
+// Define the main test class that inherits from XCTestCase
 final class Assignmate2_1Tests: XCTestCase {
-
+    
+    // This method is called before each test method, used for setup
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // Put setup code here if needed
     }
 
+    // This method is called after each test method, used for cleanup
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        // Put teardown/cleanup code here if needed
     }
 
+    // Example of a functional test case
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        // This is where you write your actual test cases
+        // Use XCTest assertions to verify the correctness of your code
+        
+        // You can use XCTAsssert functions (e.g., XCTAssertEqual, XCTAssertTrue)
+        // to check if your code produces the expected results.
+        
+        // Any test case marked as throws is expected to fail if it encounters an uncaught error.
+        // You can use XCTFail() to explicitly fail a test case.
     }
 
+    // Example of a performance test case
     func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        // This is an example of a performance test case
+        // You can use this for measuring the execution time of specific code
+        
+        // The self.measure { } block allows you to measure the time it takes to execute
+        // a particular piece of code.
     }
-
 }
