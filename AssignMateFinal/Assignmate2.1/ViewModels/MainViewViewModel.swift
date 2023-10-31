e2736bb5bfa699cd977c381b3baef17b5f79ee29
lukeborozan <147724303+lukeborozan@users.noreply.github.com>
2023-10-30 21:06:43 -0400

    MainViewViewModel.swift

--- AssignMateFinal/Assignmate2.1/ViewModels/MainViewViewModel.swift
+++ AssignMateFinal/Assignmate2.1/ViewModels/MainViewViewModel.swift
@@ -1,23 +1,23 @@
-//
-//  MainViewViewModel.swift
-//  AssignMate
-//
-//  Created by Jesse Chhina on 10/13/23.
-//
+
 import FirebaseAuth
 import Foundation
 
-class MainViewViewModel: ObservableObject{
+// A view model class for managing the main view
+class MainViewViewModel: ObservableObject {
     @Published var currentUserId: String = ""
     private var handler: AuthStateDidChangeListenerHandle?
     
-    init(){
+    init() {
+        // Add an authentication state change listener
         self.handler = Auth.auth().addStateDidChangeListener { [weak self] _, user in
-            DispatchQueue.main.async{
+            DispatchQueue.main.async {
+                // Update the current user's ID when the authentication state changes
                 self?.currentUserId = user?.uid ?? ""
             }
         }
     }
+    
+    // Check if a user is signed in
     public var isSignedIn: Bool {
         return Auth.auth().currentUser != nil
     }
