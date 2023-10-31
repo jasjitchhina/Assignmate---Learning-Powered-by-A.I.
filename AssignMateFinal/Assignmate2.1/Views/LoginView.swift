37484ad491c36ea561a74b4e2117bcb8376e1af2
lukeborozan <147724303+lukeborozan@users.noreply.github.com>
2023-10-30 21:34:20 -0400

    LoginView.swift

--- AssignMateFinal/Assignmate2.1/Views/LoginView.swift
+++ AssignMateFinal/Assignmate2.1/Views/LoginView.swift
@@ -1,64 +1,57 @@
-//
-//  LoginView.swift
-//  AssignMate
-//
-//  Created by Jesse Chhina on 10/13/23.
-//
-
 import SwiftUI
 
+// A view for user login
 struct LoginView: View {
     @StateObject var viewModel = LoginViewViewModel()
     
     var body: some View {
         NavigationStack{
             VStack{
-                //header
+                // Header view with the app title, subtitle, angle, and background color
                 HeaderView(title: "AssignMate",
                            subtitle: "Supercharge your learning.",
                            angle: 15,
                            background: .purple)
                 
-                //login fields
+                // Login form
                 Form{
+                    // Display error message if it's not empty
                     if !viewModel.errorMessage.isEmpty {
                         Text(viewModel.errorMessage)
                             .foregroundStyle(Color.red)
                     }
                     
-                    
+                    // Email address text field
                     TextField("Email Address", text: $viewModel.email)
                         .textFieldStyle(DefaultTextFieldStyle())
                         .autocorrectionDisabled()
                         .autocapitalization(.none)
                     
+                    // Password secure text field
                     SecureField("Password", text: $viewModel.password)
                         .textFieldStyle(DefaultTextFieldStyle())
                     
-                    AMButton(title: "Log In", 
-                             background: .blue)
-                    {
+                    // Login button
+                    AMButton(title: "Log In", background: .blue) {
                         viewModel.login()
                     }
-                    
                 }
+                
+                // Create account section
                 VStack{
                     Text("New around here?")
                     
-                    NavigationLink("Create An Account",
-                                   destination: RegisterView())
+                    // Navigation link to the registration view
+                    NavigationLink("Create An Account", destination: RegisterView())
                 }
-                
-                //create account
-                
                 
                 Spacer()
             }
         }
-        
     }
 }
 
 #Preview {
+    // Example usage of the LoginView
     LoginView()
 }
