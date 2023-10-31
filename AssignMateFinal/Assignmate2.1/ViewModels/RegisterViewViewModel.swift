76f1a3935e536f184787d07d54421e1b50af1731
lukeborozan <147724303+lukeborozan@users.noreply.github.com>
2023-10-30 21:08:26 -0400

    RegisterViewViewModel.swift

--- AssignMateFinal/Assignmate2.1/ViewModels/RegisterViewViewModel.swift
+++ AssignMateFinal/Assignmate2.1/ViewModels/RegisterViewViewModel.swift
@@ -1,57 +1,63 @@
-//
-//  RegisterViewViewModel.swift
-//  AssignMate
-//
-//  Created by Jesse Chhina on 10/13/23.
-//
+
 import FirebaseFirestore
 import FirebaseAuth
 import Foundation
 
-class RegisterViewViewModel: ObservableObject{
+// A view model class for managing the registration view
+class RegisterViewViewModel: ObservableObject {
     @Published var name = ""
     @Published var email = ""
     @Published var password = ""
     
     init() {}
     
+    // Function to register a new user
     func register() {
         guard validate() else {
             return
         }
-        Auth.auth().createUser(withEmail: email, password: password) {[weak self] result, error in
-            guard let userId = result?.user.uid else{
+        
+        // Create a new user account with the provided email and password
+        Auth.auth().createUser(withEmail: email, password: password) { [weak self] result, error in
+            guard let userId = result?.user.uid else {
                 return
             }
+            // Insert user record into Firestore
             self?.insertUserRecord(id: userId)
         }
     }
     
-    private func insertUserRecord(id: String){
-        let newUser = User(id: id,
-                           name: name,
-                           email: email,
-                           joined: Date().timeIntervalSince1970)
+    // Function to insert a user record into Firestore
+    private func insertUserRecord(id: String) {
+        let newUser = User(
+            id: id,
+            name: name,
+            email: email,
+            joined: Date().timeIntervalSince1970
+        )
         
         let db = Firestore.firestore()
         db.collection("users")
             .document(id)
-            .setData(newUser.asDictionary())
-        
+            .setData(newUser.asDictionary()) // Save user data in Firestore
     }
     
+    // Function to validate user input
     private func validate() -> Bool {
         guard !name.trimmingCharacters(in: .whitespaces).isEmpty,
               !email.trimmingCharacters(in: .whitespaces).isEmpty,
-              !password.trimmingCharacters(in: .whitespaces).isEmpty else{
+              !password.trimmingCharacters(in: .whitespaces).isEmpty else {
             return false
         }
-        guard email.contains("@") && email.contains(".") else{
+        
+        guard email.contains("@") && email.contains(".") else {
             return false
         }
-        guard password.count >= 6 else{
+        
+        guard password.count >= 6 else {
             return false
         }
+        
         return true
     }
 }
