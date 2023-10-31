a2336a9690425b29a7ad20d870654335bcae9042
Jasjit Chhina <100190614+jessechhina@users.noreply.github.com>
2023-10-30 20:14:22 -0400

    Add files via upload

---
+++ AssignMateFinal/Assignmate2.1/FlashcardMaker/ScannerView.swift
@@ -0,0 +1,58 @@
+//
+//  ScannerView.swift
+//  AssignMateFLASHCARD+OCR
+//
+//  Created by Peter Borozan on 10/28/23.
+//
+
+import VisionKit
+import SwiftUI
+
+struct ScannerView: UIViewControllerRepresentable {
+    private let completionHandler: ([String]?) -> Void
+     
+    init(completion: @escaping ([String]?) -> Void) {
+        self.completionHandler = completion
+    }
+     
+    typealias UIViewControllerType = VNDocumentCameraViewController
+     
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ScannerView>) -> VNDocumentCameraViewController {
+        let viewController = VNDocumentCameraViewController()
+        viewController.delegate = context.coordinator
+        return viewController
+    }
+     
+    func updateUIViewController(_ uiViewController: VNDocumentCameraViewController, context: UIViewControllerRepresentableContext<ScannerView>) {}
+     
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(completion: completionHandler)
+    }
+     
+    final class Coordinator: NSObject, VNDocumentCameraViewControllerDelegate {
+        private let completionHandler: ([String]?) -> Void
+         
+        init(completion: @escaping ([String]?) -> Void) {
+            self.completionHandler = completion
+        }
+         
+        func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+            // Handle the scan results as you already do...
+            let recognizer = TextRecognizer(cameraScan: scan)
+            recognizer.recognizeText(withCompletionHandler: completionHandler)
+
+            // Dismiss the UIViewController
+            controller.dismiss(animated: true)
+        }
+
+         
+        func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
+            completionHandler(nil)
+        }
+         
+        func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
+            print("Document camera view controller did finish with error ", error)
+            completionHandler(nil)
+        }
+    }
+}
