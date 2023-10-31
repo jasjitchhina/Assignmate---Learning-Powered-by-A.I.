01f8cf096374caaf7045e2c4c7088afc8b724398
lukeborozan <147724303+lukeborozan@users.noreply.github.com>
2023-10-30 21:02:01 -0400

    Extensions.swift

--- AssignMateFinal/Assignmate2.1/Other/Extensions.swift
+++ AssignMateFinal/Assignmate2.1/Other/Extensions.swift
@@ -1,23 +1,18 @@
-//
-//  Extensions.swift
-//  AssignMate
-//
-//  Created by Jesse Chhina on 10/14/23.
-//
 
 import Foundation
 
-extension Encodable{
-    func asDictionary() -> [String: Any]{
-        guard let data = try? JSONEncoder().encode(self) else{
+// An extension to convert an Encodable object to a dictionary
+extension Encodable {
+    func asDictionary() -> [String: Any] {
+        guard let data = try? JSONEncoder().encode(self) else {
             return [:]
         }
         
-        do{
+        do {
             let json = try JSONSerialization.jsonObject(with: data) as? [String: Any]
             return json ?? [:]
         } catch {
-            return[:]
+            return [:]
         }
     }
 }
